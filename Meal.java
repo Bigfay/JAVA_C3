52c6184aa78ad672d79cf91f2bcf7910ce9a9290
Bigfay <fdraidi@hotmail.com>
2024-05-11 12:03:33 +0200

    pré-test

--- Meal.java
+++ Meal.java
@@ -1,28 +1,28 @@
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
+//import java.util.LinkedHashSet;
 import java.util.List;
 
 /**
- * @Overview Meal est une repr´esentation mutable d'un repas en cours de
+ * @Overview Meal est une représentation mutable d'un repas en cours de
  *           commande
  *           au restaurant Mamayi.
- *           Un Meal peut ^etre vu comme un ensemble de couples (p,q) tel que le
- *           Course 'p' a ´et´e
- *           command´e 'q' fois au sein de ce Meal. Les Course sont tri´es par
+ *           Un Meal peut être vu comme un ensemble de couples (p,q) tel que le
+ *           Course 'p' a été
+ *           commandé 'q' fois au sein de ce Meal. Les Course sont triés par
  *           ordre de commande.
- *           Une fois un Course de type "Dessert" command´e, il ne peut plus y
+ *           Une fois un Course de type "Dessert" commandé, il ne peut plus y
  *           avoir que des
  *           Course de type "Dessert".
  *
  * @specfield ordered : {(Course1, Integer1),...,(CourseX, IntegerX)} //
  *            L'ensemble
- *            des plats command´es ainsi que le nombre de fois o`u ils ont ´et´e
- *            command´es
+ *            des plats commandés ainsi que le nombre de fois où ils ont été
+ *            commandés
  *
  * @derivedfield price : real // the prix total du repas
  *               // price = Somme(p.price*q) o`u (p,q) in ordered
  *
- * @invariant : une fois un Course "Dessert" command´e, il n'y a que des Course
+ * @invariant : une fois un Course "Dessert" commandé, il n'y a que des Course
  *            "Dessert"
  */
 public class Meal {
@@ -30,10 +30,10 @@ public class Meal {
     List<Course> ordered;
     boolean dessertOrdered;
 
-    //[...]
+    // [...]
 
     /**
-     * @effects cr´ee un Meal vide pr^et `a recevoir des plats et desserts
+     * @effects crée un Meal vide prêt à recevoir des plats et desserts
      */
     public Meal() {
         ordered = new ArrayList<Course>();
@@ -46,9 +46,9 @@ public class Meal {
      * @requires c is not null
      * @modifies this
      * @effects si c est un dessert, indique que nous sommes au dessert et rajoute
-     *          c `a ordered; sinon v´erifie que les plats sont autoris´es et
+     *          c à ordered; sinon v´erifie que les plats sont autorisés et
      *          rajoute
-     *          alors c `a ordered;
+     *          alors c à ordered;
      */
     public void order(Course c) {
         if (c.isDessert()) {
@@ -60,21 +60,22 @@ public class Meal {
             }
         }
     }
-    //[...]
+    // [...]
 
-/**
- * @requires c is not null
- * @modifies this
- * @effects si c est un dessert, indique que nous sommes au dessert et rajoute
- * number fois c `a ordered; sinon v´erifie que les plats sont autoris´es et rajoute
- * alors number fois c `a ordered;
- */
- public void order(Course c, int number) {
- for(int i=1;i<=number;i++){
- order(c);
- }
- }
+    /**
+     * @requires c is not null
+     * @modifies this
+     * @effects si c est un dessert, indique que nous sommes au dessert et rajoute
+     *          number fois c à ordered; sinon vérifie que les plats sont autorisés
+     *          et rajoute
+     *          alors number fois c à ordered;
+     */
+    public void order(Course c, int number) {
+        for (int i = 1; i <= number; i++) {
+            order(c);
+        }
+    }
 
-    //[...]
+    // [...]
 
 }
---
+++ MealTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MealTest {
+    private static final int SOME_COEF = 2;
+
+    @Test
+    public void testMeal() {
+        // assertEquals("La somme de 1 et 2 devrait être 3", 3, Calculateur.somme(1,
+        // 2));
+        // assertEquals("coucou", 5, Calculateur.somme(2, 3));
+
+        /*
+         * Meal = {(”Poulet coco”, 4)} Meal = {(”Poulet coco”, 4),(”Riz”, 4)}
+         * c = ”Riz”
+         * number = 4
+         */
+        assertEquals(course abc, abc);
+    }
+}
